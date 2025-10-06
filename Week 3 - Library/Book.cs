7008aa3ff742adf30ea04fd434ee37adbe0fb7d2
b01033030 <Cunningham-R36@ulster.ac.uk>
2025-10-06 15:51:25 +0100

    week 3 - classes and objects

--- Week 3 - Library/Book.cs
+++ Week 3 - Library/Book.cs
@@ -8,15 +8,32 @@ namespace Week_3___Library
 {
     class Book
     {
-        string Title;
-        string Author;
-        string ISBN;
+        private string title; //variable
+        private string author; //variable
+        private string isbn; //variable
+
+        //title property allows acces to the private title variable
+        public string Title
+        {
+            get { return title; } //get method
+            set { title = value; } //set method
+        }
+        public string Author
+        {
+            get { return author; } //get method
+            set { author = value; } //set method
+        }
+        public string ISBN
+        {
+            get { return isbn; } //get method
+            set { isbn = value; } //set method
+        }
 
         public Book(string bookTitle, string bookAuthor, string bookISBN)
         {
-            Title = bookTitle;
-            Author = bookAuthor;
-            ISBN = bookISBN;
+            this.Title = bookTitle;
+            this.Author = bookAuthor;
+            this.ISBN = bookISBN;
         }
 
         public void DisplayInfo()
