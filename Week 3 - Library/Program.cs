9ef63a7824a69d459dadb066f91035534803539f
b01033030 <Cunningham-R36@ulster.ac.uk>
2025-10-06 15:42:02 +0100

    Changed code to have access modifiers

--- Week 3 - Library/Program.cs
+++ Week 3 - Library/Program.cs
@@ -1,29 +1,11 @@
 ﻿
 using System;
-
-class Book
+using Week_3___Library;
+class Program
 {
-    string Title;
-    string Author;
-    string ISBN;
-
-    public Book(string bookTitle, string bookAuthor, string bookISBN)
-    {
-        Title = bookTitle;
-        Author = bookAuthor;
-        ISBN = bookISBN;
-    }
-    void DisplayInfo()
-    {
-        Console.WriteLine($"Book title: {Title}");
-        Console.WriteLine($"Book Author: {Author}");
-        Console.WriteLine($"Book ISBN: {ISBN}");
-        Console.WriteLine();
-    }
+    
     static void Main(string[] args)
     {
-
-
         Book book = new Book("C# for beginners", "Bill Gates", "1234567");
 
         book.DisplayInfo(); 
